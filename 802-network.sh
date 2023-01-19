5c41c0bf869904a4cd480776932fd6c70cf295a9
maccampbell <campbellmatthewa@gmail.com>
2023-01-19 11:08:05 -0500

    changing stderr/stdout

--- 802-network.sh
+++ 802-network.sh
@@ -8,8 +8,8 @@
 #SBATCH --time=10:05:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=maccampbell@ucdavis.edu
-#SBATCH --output=outputs/801/net.out # File to which STDOUT will be written
-#SBATCH --error=outputs/801/net.err # File to which STDERR will be written
+#SBATCH --output=outputs/802/net.out # File to which STDOUT will be written
+#SBATCH --error=outputs/802/net.err # File to which STDERR will be written
 
 
 module load julia/1.8.2     
