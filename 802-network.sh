7851a10540bcb5f3fad64faaca360af15335af38
maccampbell <campbellmatthewa@gmail.com>
2023-01-20 12:58:50 -0500

    extending runtime for phylogeny

--- 802-network.sh
+++ 802-network.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=4G
-#SBATCH --time=10:05:00
+#SBATCH --time=24:05:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=maccampbell@ucdavis.edu
 #SBATCH --output=outputs/802/net.out # File to which STDOUT will be written
@@ -14,4 +14,4 @@
 
 module load julia/1.8.2     
 
-julia -p 8 802-network.jl
+julia -p 10 802-network.jl
